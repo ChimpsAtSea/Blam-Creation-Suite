e01f6b15f32c0ec1d237f6b858e770da6714825b
theTwist84 <lee.foulger.94@gmail.com>
2019-08-08 15:23:36 +0100

    Added more tls offsets

--- HaloShared/HaloReach.h
+++ HaloShared/HaloReach.h
@@ -5,11 +5,11 @@
 enum e_tls_offset
 {
 	// tls offsets for version 1.887.0.0 of haloreach.dll
-	// unknown
+	// 0x0
 	_tls_offset_object_header_data = 0x8,
 	_tls_offset_player_data = 0x18,
 	_tls_offset_game_engine_globals = 0x20,
-	// unknown
+	// 0x28
 	_tls_offset_ai_globals = 0x30,
 	_tls_offset_prop_ref_data = 0x38,
 	_tls_offset_hs_thread_deterministic_data = 0x40,
@@ -22,8 +22,8 @@ enum e_tls_offset
 	_tls_offset_cinematic_new_globals = 0x78,
 	_tls_offset_sound_data = 0x90,
 	_tls_offset_incident_globals = 0x98,
-	// unknown
-	// unknown
+	_tls_offset_preferences = 0xA0,
+	_tls_offset_clump_data = 0xA8,
 	_tls_offset_flocks_data = 0xB0,
 	_tls_offset_render_texture_camera_globals = 0xB8,
 	_tls_offset_game_time_globals = 0xC0,
@@ -41,9 +41,9 @@ enum e_tls_offset
 	_tls_offset_boids_data = 0x118,
 	_tls_offset_players_globals = 0x120,
 	_tls_offset_main_gamestate_timing_data = 0x128,
-	// unknown
+	// 0x130
 	_tls_offset_players_per_map_globals = 0x138,
-	// unknown
+	_tls_offset_fire_team_data = 0x140,
 	_tls_offset_looping_sound_data = 0x148,
 	_tls_offset_player_mapping_globals = 0x150,
 	_tls_offset_joint_data = 0x158,
@@ -58,9 +58,9 @@ enum e_tls_offset
 	_tls_offset_campaign_metagame = 0x1A0,
 	_tls_offset_effect_location_data = 0x1A8,
 	_tls_offset_recycling_groups = 0x1B0,
-	// unknown
+	_tls_offset_ai_players = 0x1B8,
 	_tls_offset_vocalization_record = 0x1C0,
-	// unknown
+	_tls_offset_multiplayer_sound_globals = 0x1C8,
 	_tls_offset_impact_data = 0x1D0,
 	_tls_offset_player_effect_globals = 0x1D8,
 	_tls_offset_collision_hierarchy_element_data = 0x1E0,
@@ -72,14 +72,14 @@ enum e_tls_offset
 	_tls_offset_game_sound_globals = 0x208,
 	_tls_offset_cached_object_render_states = 0x210,
 	_tls_offset_device_groups_data = 0x218,
-	// unknown
+	_tls_offset_simulation_debug_globals = 0x220,
 	_tls_offset_objectives_data = 0x228,
 	_tls_offset_ai_sync_action_arranger_data = 0x230,
 	_tls_offset_swarm_data = 0x238,
 	_tls_offset_hs_runtime_globals = 0x240,
 	_tls_offset_object_scripting_state = 0x248,
 	_tls_offset_particle_system_data_array = 0x250,
-	// unknown
+	// 0x258
 	_tls_offset_effect_counts = 0x260,
 	_tls_offset_breakable_surface_set_broken_event = 0x268,
 	_tls_offset_cloth_data = 0x270,
@@ -90,7 +90,7 @@ enum e_tls_offset
 	_tls_offset_widget_data = 0x298,
 	_tls_offset_local_game_engine_globals = 0x2A0,
 	_tls_offset_particle_emitter_data_array = 0x2A8,
-	// unknown
+	_tls_offset_particle_location_data_array = 0x2B0,
 	_tls_offset_rumble_globals = 0x2B8,
 	_tls_offset_havok_proxy_data = 0x2C0,
 	_tls_offset_damage_globals = 0x2C8,
@@ -112,7 +112,7 @@ enum e_tls_offset
 	_tls_offset_hud_camera_globals = 0x340,
 	_tls_offset_decal_system = 0x348,
 	_tls_offsetemory_pool_address = 0x350,
-	// unknown
+	// 0x358
 	_tls_offset_prop_search_data = 0x360,
 	_tls_offset_bbsquad = 0x368,
 	_tls_offset_effect_lightprobe_slot_data = 0x370,
@@ -123,7 +123,7 @@ enum e_tls_offset
 	_tls_offset_hs_global_data = 0x398,
 	_tls_offset_object_schedule_globals = 0x3A0,
 	_tls_offset_swarm_spawners = 0x3A8,
-	// unknown
+	_tls_offset_weather_state_deterministic = 0x3B0,
 	_tls_offset_light_volume_location = 0x3B8,
 	_tls_offset_memory_pool = 0x3C0,
 	_tls_offset_cheap_particle_emitter_from_update_thread_data = 0x3C8,
@@ -139,7 +139,7 @@ enum e_tls_offset
 	_tls_offset_antenna_data = 0x410,
 	_tls_offset_object_render_globals = 0x418,
 	_tls_offset_light_volume = 0x420,
-	// unknown
+	_tls_offset_lasing_data = 0x428,
 	_tls_offset_ai_cue_data = 0x430,
 	_tls_offset_water_interaction_event = 0x438,
 	_tls_offset_shield_render_cache_data = 0x440,
@@ -151,7 +151,7 @@ enum e_tls_offset
 	_tls_offset_ai_reference_frame_data = 0x470,
 	_tls_offset_beam_gpu_data_array = 0x478,
 	_tls_offset_light_volume_gpu_data_array = 0x480,
-	// unknown
+	// 0x488
 	_tls_offset_campaign_metagame_secondary = 0x490,
 	_tls_offset_performance_runtime_definition_data = 0x498,
 	_tls_offset_contrail_gpu__s_row_data_array = 0x4A0,
@@ -163,7 +163,7 @@ enum e_tls_offset
 	_tls_offset_beam_gpu__s_row_data_array = 0x4D0,
 	_tls_offset_light_volume_gpu__s_row_data_array = 0x4D8,
 	_tls_offset_weather_state_render = 0x4E0,
-	// unknown
+	_tls_offset_contrail_profile = 0x4E8,
 	_tls_offset_sound_class_data = 0x4F0,
 	_tls_offset_game_sound_multithreading_globals = 0x4F8,
 
@@ -180,7 +180,7 @@ enum e_tls_offset
 	_tls_offset_player_control_globals_deterministic = 0x550,
 	_tls_offset_effect_message_queue = 0x558,
 	_tls_offset_object_placement_globals = 0x560,
-	// unknown
+	_tls_offset_bbsquad_unit = 0x568,
 	_tls_offset_team_dialogue_state = 0x570,
 	_tls_offset_simulated_input_globals = 0x578,
 	_tls_offset_future_development = 0x580,
@@ -192,43 +192,51 @@ enum e_tls_offset
 	_tls_offset_deterministic_random_seed_ptr = 0x5B0,
 	_tls_offset_object_message_queue = 0x5B8,
 	_tls_offset_chud_persistent_global_data = 0x5C0,
-	// unknown
+	_tls_offset_effect_lightprobe_data = 0x5C8,
 	_tls_offset_scenario_interpolator_globals = 0x5D0,
 	_tls_offset_muffin_data = 0x5D8,
+	_tls_offset_spawner_globals = 0x5E0,
 	_tls_offset_observer_gamestate_globals = 0x5E8,
 	_tls_offset_chud_persistent_user_data = 0x5F0,
+	// 0x5F8
 
-	// unknown
-	// unknown
-	// unknown
-	// unknown
+	// 0x600
+	// 0x608
+	// 0x610
+	_tls_offset_render_game_globals = 0x618,
 	_tls_offset_player_effects_globals = 0x620,
 	_tls_offset_scenario_soft_ceilings = 0x628,
 	_tls_offset_breakable_surface_redirection_globals = 0x630,
 	_tls_offset_tasks_data = 0x638,
-	// unknown
+	// 0x640
 	_tls_offset_game_heat_globals = 0x648,
 	_tls_offset_active_portal_bitvectors = 0x650,
 	_tls_offset_cinematic_light_globals = 0x658,
+	_tls_offset_game_engine_render_globals = 0x660,
+	// 0x668
+	// 0x670
+	// 0x678
 	_tls_offset_vision_mode_state = 0x680,
-	// unknown
+	_tls_offset_first_person_weapon_orientations = 0x688,
 	_tls_offset_observer_globals = 0x690,
 	_tls_offset_cloth_globals = 0x698,
 	_tls_offset_object_render_data = 0x6A0,
 	_tls_offset_first_person_weapons = 0x6A8,
 	_tls_offset_havok_contact_point_globals = 0x6B8,
-	// unknown
-	// unknown
-	// unknown
-	// unknown
-	// unknown
-	// unknown
+	// 0x6C0
+	// 0x6C8
+	// 0x6D0
+	// 0x6D8
+	// 0x6E0
+	// 0x6E8
+	// 0x6F0
+	// 0x6F8
 
-	// unknown
+	// 0x700
 	_tls_offset_user_widgets = 0x708,
-	// unknown
-	// unknown
-	// unknown
+	// 0x710
+	// 0x718
+	// 0x720
 	_tls_offset_usability_globals_backup = 0x728,
 	_tls_offset_start_menu_campaign_objectives = 0x730
 	// unsure if more exist after 0x730
