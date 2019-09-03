b6d553caf5aabd2614400244d030e331a734376c
Haydn Trigg <me@haydntrigg.com>
2019-09-03 02:12:01 +0100

    GameLauncher::RegisterTerminationValue for Halo Reach

--- HaloReach/HaloReach.cpp
+++ HaloReach/HaloReach.cpp
@@ -367,6 +367,8 @@ void init_halo_reach(HaloGameID gameID)
 	DataReferenceBase::InitTree(gameID);
 	FunctionHookBase::InitTree(gameID);
 	
+	GameLauncher::RegisterTerminationValue(*reinterpret_cast<char*>(byte_183984DE4.ptr()));
+
 	if (gameID == HaloGameID::HaloReach_2019_Jun_24)
 	{
 		g_shell_command_line = GetCommandLineA();
