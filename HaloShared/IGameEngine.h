6a8dda815d11544e65590d6270e62af43e99efe0
Haydn Trigg <me@haydntrigg.com>
2019-09-07 16:14:25 +0100

    Update IGameEngine.h

--- HaloShared/IGameEngine.h
+++ HaloShared/IGameEngine.h
@@ -97,7 +97,18 @@ static constexpr size_t s_game_launch_data_size = sizeof(s_game_launch_data);
 static_assert(s_game_launch_data_size == 0x2B7C8, "");
 static_assert(offsetof(s_game_launch_data, s_game_launch_data::pGameHandle) == 0x2B740, "");
 
-class IGameEngine
+//// for some reason MSVC doesn't respect the correct size of alignment of this structure
+//// this is supposed to be a SLIST_HEADER
+//struct SLIST_HEADER_Explicit 
+//{
+//public:
+//	SLIST_HEADER& Get() { return *reinterpret_cast<SLIST_HEADER*>(data); }
+//private:
+//	uint64_t data[2];
+//};
+//static_assert(sizeof(SLIST_HEADER_Explicit) == sizeof(SLIST_HEADER), "SLIST_HEADER_Explicit should match size of SLIST_HEADER");
+
+class IGameEngine2
 {
 public:
 	virtual __int64 __fastcall InitGraphics(ID3D11Device*, ID3D11DeviceContext*, IDXGISwapChain*, IDXGISwapChain*) = 0;
@@ -112,8 +123,98 @@ public:
 	virtual __int64 __fastcall Member09(const char*) = 0;
 	virtual __int64 __fastcall Member10() = 0;
 
-	DWORD unknown0;
-	float unknown1;
-	char unknown2[454];
+	DWORD unknown8;
+	float unknownC;
+	char unknown10[1056];
+	SLIST_HEADER header430;
+	SLIST_HEADER header440;
 };
 
+class IGameEngine
+{
+public:
+	__forceinline __int64 __fastcall InitGraphics(ID3D11Device* pDevice, ID3D11DeviceContext* pDeviceContext, IDXGISwapChain* pSwapchain, IDXGISwapChain* pFallbackSwapchain) 
+	{ 
+		return impl.__vftable->InitGraphics(this, pDevice, pDeviceContext, pSwapchain, pFallbackSwapchain);
+	}
+	HANDLE __fastcall InitThread(class IGameEngineHost* pGameEngineHost, s_game_launch_data* pGameLaunchData)
+	{
+		return impl.__vftable->InitThread(this, pGameEngineHost, pGameLaunchData);
+	}
+	__int64 __fastcall Member02(int a1, _QWORD* a2)
+	{
+		return impl.__vftable->Member02(this, a1, a2);
+	}
+	__int64 __fastcall Destructor()
+	{
+		return impl.__vftable->Destructor(this);
+	}
+	void __fastcall Member04()
+	{
+		return impl.__vftable->Member04(this);
+	}
+	void __fastcall Member05()
+	{
+		return impl.__vftable->Member05(this);
+	}
+	void __fastcall Member06()
+	{
+		return impl.__vftable->Member06(this);
+	}
+	void __fastcall Member07()
+	{
+		return impl.__vftable->Member07(this);
+	}
+	void __fastcall Member08()
+	{
+		return impl.__vftable->Member08(this);
+	}
+	__int64 __fastcall Member09(const char* a1)
+	{
+		return impl.__vftable->Member09(this, a1);
+	}
+	__int64 __fastcall Member10()
+	{
+		return impl.__vftable->Member10(this);
+	}
+
+private:
+	struct /*VFT*/ IGameEngine_vtbl
+	{
+		__int64(__fastcall* InitGraphics)(IGameEngine*, struct ID3D11Device*, struct ID3D11DeviceContext*, struct IDXGISwapChain*, struct IDXGISwapChain*);
+		HANDLE(__fastcall* InitThread)(IGameEngine*, struct IGameEngineHost*, struct s_game_launch_data*);
+		__int64(__fastcall* Member02)(IGameEngine*, int, _QWORD*);
+		__int64(__fastcall* Destructor)(IGameEngine*);
+		void(__fastcall* Member04)(IGameEngine*);
+		void(__fastcall* Member05)(IGameEngine*);
+		void(__fastcall* Member06)(IGameEngine*);
+		void(__fastcall* Member07)(IGameEngine*);
+		void(__fastcall* Member08)(IGameEngine*);
+		__int64(__fastcall* Member09)(IGameEngine*, const char*);
+		__int64(__fastcall* Member10)(IGameEngine*);
+	};
+
+	struct IGameEngineData
+	{
+		IGameEngine_vtbl* __vftable /*VFT*/;
+		DWORD unknown8;
+		float unknownC;
+		char unknown10[1056];
+		SLIST_HEADER header430;
+		SLIST_HEADER header440;
+		char unknown450[16];
+	};
+	static constexpr size_t unknown8Offset = offsetof(IGameEngineData, IGameEngineData::unknown8);
+	static_assert(unknown8Offset == 0x8, "unknown8 is incorrect offset");
+	static constexpr size_t header430Offset = offsetof(IGameEngineData, IGameEngineData::header430);
+	static_assert(header430Offset == 0x430, "header430 is incorrect offset");
+	static constexpr size_t header440Offset = offsetof(IGameEngineData, IGameEngineData::header440);
+	static_assert(header440Offset == 0x440, "header440 is incorrect offset");
+	static constexpr size_t IGameEngineDataSize = sizeof(IGameEngineData);
+	static_assert(IGameEngineDataSize == 0x460, "IGameEngineData is incorrect size");
+
+	IGameEngineData impl;
+};
+
+static constexpr size_t IGameEngineSize = sizeof(IGameEngine);
+static_assert(IGameEngineSize == 0x460, "IGameEngine is incorrect size");
