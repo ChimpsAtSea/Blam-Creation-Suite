ab9ac69ce5ec7a6d939b921f6eeb1f613c1448ca
Haydn Trigg <me@haydntrigg.com>
2020-01-17 01:42:53 +0000

    Render DirectX 11 to Xaml Islands

--- MyApp/MainUserControl.cpp
+++ MyApp/MainUserControl.cpp
@@ -1,30 +1,65 @@
 ﻿#include "pch.h"
 #include "MainUserControl.h"
+#include "OpusUIPrototypeMain.h"
 
 using namespace winrt;
 using namespace winrt::Windows::UI::Xaml;
+using namespace Concurrency;
 
 namespace winrt::MyApp::implementation
 {
-    MainUserControl::MainUserControl()
-    {
-        InitializeComponent();
+	MainUserControl::MainUserControl()
+	{
+		InitializeComponent();
 
-        ::winrt::Windows::UI::Xaml::Controls::SwapChainPanel swapChainPanel = SwapChainPanelControl();
+		::winrt::Windows::UI::Xaml::Controls::SwapChainPanel swapChainPanel = SwapChainPanelControl();
 
-        m_deviceResources = std::make_shared<DX::DeviceResources>();
-        m_deviceResources->SetSwapChainPanel(swapChainPanel);
+		m_deviceResources = std::make_shared<DX::DeviceResources>();
+		m_deviceResources->SetSwapChainPanel(swapChainPanel);
+
+		m_main = std::unique_ptr<OpusUIPrototype::OpusUIPrototypeMain>(new OpusUIPrototype::OpusUIPrototypeMain(m_deviceResources));
+
+		//// Register event handlers for page lifecycle.
+		//CoreWindow^ window = Window::Current->CoreWindow;
+
+		//window->VisibilityChanged +=
+		//	ref new TypedEventHandler<CoreWindow^, VisibilityChangedEventArgs^>(this, &DirectXPage::OnVisibilityChanged);
+
+		//DisplayInformation^ currentDisplayInformation = DisplayInformation::GetForCurrentView();
+
+		//currentDisplayInformation->DpiChanged +=
+		//	ref new TypedEventHandler<DisplayInformation^, Object^>(this, &DirectXPage::OnDpiChanged);
+
+		//currentDisplayInformation->OrientationChanged +=
+		//	ref new TypedEventHandler<DisplayInformation^, Object^>(this, &DirectXPage::OnOrientationChanged);
+
+		//DisplayInformation::DisplayContentsInvalidated +=
+		//	ref new TypedEventHandler<DisplayInformation^, Object^>(this, &DirectXPage::OnDisplayContentsInvalidated);
+
+		//swapChainPanel->CompositionScaleChanged +=
+		//	ref new TypedEventHandler<SwapChainPanel^, Object^>(this, &DirectXPage::OnCompositionScaleChanged);
+
+		swapChainPanel.SizeChanged([=](winrt::Windows::Foundation::IInspectable const& sender, winrt::Windows::UI::Xaml::SizeChangedEventArgs const& e) {
+
+			critical_section::scoped_lock lock(m_main->GetCriticalSection());
+			m_deviceResources->SetLogicalSize(e.NewSize());
+			m_main->CreateWindowSizeDependentResources();
+
+			});
+	}
 
 
-    }
 
-    winrt::hstring MainUserControl::MyProperty()
-    {
-        return userControl().MyProperty();
-    }
 
-    void MainUserControl::MyProperty(winrt::hstring value)
-    {
-        userControl().MyProperty(value);
-    }
+
+
+	winrt::hstring MainUserControl::MyProperty()
+	{
+		return userControl().MyProperty();
+	}
+
+	void MainUserControl::MyProperty(winrt::hstring value)
+	{
+		userControl().MyProperty(value);
+	}
 }
--- MyApp/MainUserControl.h
+++ MyApp/MainUserControl.h
@@ -9,6 +9,11 @@
 #include "winrt/Windows.UI.Xaml.Interop.h"
 #include "MainUserControl.g.h"
 
+namespace OpusUIPrototype
+{
+	class OpusUIPrototypeMain;
+}
+
 namespace winrt::MyApp::implementation
 {
     struct MainUserControl : MainUserControlT<MainUserControl>
@@ -19,6 +24,8 @@ namespace winrt::MyApp::implementation
 		void MyProperty(hstring value);
 
 		std::shared_ptr<DX::DeviceResources> m_deviceResources;
+		std::unique_ptr<OpusUIPrototype::OpusUIPrototypeMain> m_main;
+
     };
 }
 
