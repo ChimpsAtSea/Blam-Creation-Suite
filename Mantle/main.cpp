12a8934af3f4d51c3bc30d3619882221085ec479
Haydn Trigg <me@haydntrigg.com>
2020-01-23 20:15:40 +0000

    WinMain Entry Point

--- Mantle/main.cpp
+++ Mantle/main.cpp
@@ -1,6 +1,12 @@
 #include "mantle-private-pch.h"
 
-int main()
+
+int WINAPI WinMain(
+	HINSTANCE hInstance,     /* [input] handle to current instance */
+	HINSTANCE hPrevInstance, /* [input] handle to previous instance */
+	LPSTR lpCmdLine,         /* [input] pointer to command line */
+	int nCmdShow             /* [input] show state of window */
+)
 {
 	void(*UICallback)() = []()
 	{
