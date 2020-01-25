805dd2e1b596376fe5d4e0d3e878f0ad8d772002
Haydn Trigg <me@haydntrigg.com>
2020-01-25 23:49:21 +0000

    Update FatalError.h

--- Shared/FatalError.h
+++ Shared/FatalError.h
@@ -5,6 +5,6 @@
 #define DEBUG_FATAL_ERROR()
 #else
 void __FatalErrorInternal(const wchar_t* pReason, const wchar_t* pFile, unsigned int line, ...);
-#define FATAL_ERROR(reason, ...) __FatalErrorInternal(reason, _CRT_WIDE(__FILE__), (unsigned)(__LINE__), ##__VA_ARGS__)
-#define DEBUG_FATAL_ERROR(reason, ...) do { if (IsDebuggerPresent()) { __FatalErrorInternal(reason, _CRT_WIDE(__FILE__), (unsigned)(__LINE__), ##__VA_ARGS__); } } while() 
+#define FATAL_ERROR(reason, ...) __FatalErrorInternal(reason, _CRT_WIDE(__FILE__), (unsigned)(__LINE__), ##__VA_ARGS__); throw
+#define DEBUG_FATAL_ERROR(reason, ...) do { if (IsDebuggerPresent()) { __FatalErrorInternal(reason, _CRT_WIDE(__FILE__), (unsigned)(__LINE__), ##__VA_ARGS__); } } while(); throw
 #endif
