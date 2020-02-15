425da6f1f26a0956068c65cd32ff1bb77e9cc114
Haydn Trigg <me@haydntrigg.com>
2020-02-15 21:00:47 +0000

    Halo 1 CreateGameEngine and InitThread Hooks

--- MCC/MCCHooks.cpp
+++ MCC/MCCHooks.cpp
@@ -21,4 +21,45 @@ FunctionHookVarArgsEx<sub_141806A64_offset, const wchar_t* __fastcall (__int64 a
 } };
 
 
+intptr_t CreateGameEngine_offset(EngineVersion engineVersion, BuildVersion buildVersion)
+{
+	if (engineVersion == EngineVersion::Halo1)
+	{
+		switch (buildVersion)
+		{
+		case BuildVersion::Build_1_1350_0_0: return 0x18008D2C0;
+		}
+	}
+	return ~intptr_t();
+}
+FunctionHookVarArgsEx<CreateGameEngine_offset, __int64 __fastcall (QWORD * a1)> CreateGameEngine = { "CreateGameEngine", [](QWORD* a1)
+{
+	return CreateGameEngine(a1);
+} };
+
+intptr_t InitThread_offset(EngineVersion engineVersion, BuildVersion buildVersion)
+{
+	if (engineVersion == EngineVersion::Halo1)
+	{
+		switch (buildVersion)
+		{
+		case BuildVersion::Build_1_1350_0_0: return 0x18008C550;
+		}
+	}
+	return ~intptr_t();
+}
+FunctionHookVarArgsEx<InitThread_offset, HANDLE __fastcall (IGameEngine*, IGameEngineHost*, GameContext*)> InitThread = { "InitThread", [](IGameEngine* pGameEngine, IGameEngineHost* pGameEngineHost, GameContext* pGameContext)
+{
+	return InitThread(pGameEngine, pGameEngineHost, pGameContext);
+} };
+
+
+
+
+
+
+
+
+
+
 
