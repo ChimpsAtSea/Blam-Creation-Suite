d8e714c1271285b4c1e47d644b3b39a27ce83e2d
Haydn Trigg <me@haydntrigg.com>
2020-02-26 17:13:31 +0000

    Removed SampleUserControl

--- SampleUserControl/ReadMe.md
+++
@@ -1,4 +0,0 @@
-# WinRT Conmponent with UWP XAML User Controls
-
-This project is a UWP WinRT Component that contains a UWP XAML User Controls.
-
--- SampleUserControl/pch.h
+++
@@ -1,5 +0,0 @@
-﻿#pragma once
-#include <unknwn.h>
-#include <winrt/Windows.Foundation.h>
-#include <winrt/Windows.UI.Xaml.h>
-#include <winrt/Windows.UI.Xaml.Controls.Primitives.h>
