d20fab3c640409ee35e7f292d1dc057ac5ba4918
theTwist84 <lee.foulger.94@gmail.com>
2020-05-28 02:48:44 +0100

    Added Halo4Lib

---
+++ Game/Halo4Lib/halo4lib-private-pch.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#ifdef __cplusplus
+
+#include <Platform\platform-public-pch.h>
+#include <Versioning\versioning-public-pch.h>
+#include <Shared\shared-public-pch.h>
+#include <TagDefinitions\tagdefinitions-public-pch.h>
+#include <MantleGen\mantlegen-public-pch.h>
+#include <MantleLib\mantlelib-public-pch.h>
+#include <SymbolsLib\symbolslib-public-pch.h>
+#include <GameFramework\gameframework-public-pch.h>
+#include <Ketchup\ketchup-public-pch.h>
+
+#include "halo4lib-public-pch.h"
+
+#include "halo4_engine_state_command.h"
+
+#endif
---
+++ Game/Halo4Lib/halo4lib-public-pch.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#ifdef _WIN64
+
+#include "halo4_game_host.h"
+
+#endif
