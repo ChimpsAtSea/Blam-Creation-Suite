0e321d86833f1fff4ace8338bfa938b07c57c823
Haydn Trigg <me@haydntrigg.com>
2020-06-18 04:46:12 +0100

    Update halo3_cache_file.cpp

--- Tags/MandrillLib/CacheInterface/CacheVersions/halo3/halo3_cache_file.cpp
+++ Tags/MandrillLib/CacheInterface/CacheVersions/halo3/halo3_cache_file.cpp
@@ -40,6 +40,11 @@ c_halo3_cache_file::c_halo3_cache_file(const std::wstring& map_filepath) :
 	}
 
 	init_sorted_instance_lists();
+
+	for (c_gen3_tag_interface& tag_interface : c_reference_loop(reinterpret_cast<c_gen3_tag_interface* const*>(get_tag_interfaces()), get_tag_count()))
+	{
+		tag_interface.validate();
+	}
 }
 
 c_halo3_cache_file::~c_halo3_cache_file()
