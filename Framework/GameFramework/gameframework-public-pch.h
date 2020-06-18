a726e21dde1ce6aa65a823b0a70ede47cc077b15
Haydn Trigg <me@haydntrigg.com>
2020-06-18 03:04:30 +0100

    Update gameframework-public-pch.h

--- Framework/GameFramework/gameframework-public-pch.h
+++ Framework/GameFramework/gameframework-public-pch.h
@@ -5,6 +5,5 @@
 #include "ApplicationReflection/ApplicationReflection.h"
 #include "RuntimeManipulation/RuntimeManipulation.h"
 #include "is_valid.h"
-#include "Settings/settings_legacy.h"
 #include "platform_configuration.h"
 #include "session_manager.h"
