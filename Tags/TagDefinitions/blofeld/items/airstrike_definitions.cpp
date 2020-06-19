27954b80abb66a66ebe973d9227467082866e28e
Akarias <Akarias@users.noreply.github.com>
2020-06-19 23:22:45 +0100

    [Reach] airstrike_definitions.cpp (#16)
    * [Reach] collision_damage_definitions.cpp
    
    Completed
    
    * [Reach] airstrike_definitions.cpp
    
    Done

--- Tags/TagDefinitions/blofeld/items/airstrike_definitions.cpp
+++ Tags/TagDefinitions/blofeld/items/airstrike_definitions.cpp
@@ -21,9 +21,20 @@ namespace blofeld
 		{ _field_real, "launch cooldown:seconds#seconds to wait before the next launch will be ready" },
 		{ _field_real, "launch duration:seconds#how long a launch should take to fire all rounds" },
 		{ _field_long_integer, "shots per launch#number of rounds to fire per launch" },
+
+		{ _field_version_equal, _engine_type_haloreach },
+		{ _field_long_integer, "default shot count" },
+
 		{ _field_tag_reference, "fire effect#the effect that will be created when the strike fires.", &global_effect_reference },
+
+		{ _field_version_greater, _engine_type_haloreach },
 		{ _field_tag_reference, "fire effect (indoor)#the effect that will be created when the strike fires and is indoors", &global_effect_reference },
+
 		{ _field_block, "fire offsets", &airstrike_fire_location_block_block },
+
+		{ _field_version_equal, _engine_type_haloreach },
+		{ _field_tag_reference, "affected weapon", &weapon_reference },
+
 		{ _field_terminator }
 	};
 
