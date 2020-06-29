41caa0b0a33fe9a58dc88fa8cb75dd9084095195
matty45 <matthewtoo5@googlemail.com>
2020-06-29 21:38:35 +0100

    [Reach] Bunch of Tag Definitions (#33)
    * [Reach] megalogamengine_string_id_table.cpp
    
    * [Reach] game_performance_throttle_definitions.cpp & damage_effect_definitions.cpp
    
    * [Reach] Revert game_performance_throttle_definitions.cpp

--- Tags/TagDefinitions/blofeld/game/megalogamengine/megalogamengine_string_id_table.cpp
+++ Tags/TagDefinitions/blofeld/game/megalogamengine/megalogamengine_string_id_table.cpp
@@ -24,7 +24,10 @@ namespace blofeld
 	TAG_STRUCT(megalo_string_id_table_struct_definition)
 	{
 		{ _field_block, "megalo string_ids", &megalo_string_id_block_block },
+
+		{ _field_version_greater, _engine_type_haloreach },
 		{ _field_block, "megalo string_ids sprite mapping", &megalo_string_id_to_sprite_index_block_block },
+		
 		{ _field_terminator }
 	};
 
