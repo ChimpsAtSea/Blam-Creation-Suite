40b268a774f9d47df60864f77cadc276ee08de9c
Haydn Trigg <me@haydntrigg.com>
2020-06-29 23:01:03 +0100

    [Reach[ decal_system

--- Tags/TagDefinitions/blofeld/effects/decal_definitions.cpp
+++ Tags/TagDefinitions/blofeld/effects/decal_definitions.cpp
@@ -10,15 +10,29 @@ namespace blofeld
 	{
 		{ _field_string_id, "decal name^" },
 		{ _field_long_flags, "flags", &decal_flags },
+
+		{ _field_version_less_or_equal, _engine_type_haloreach, 2 },
+		{ _field_explanation, "Render Method" },
+		{ _field_struct, "render method", &render_method_struct_definition_struct_definition },
+
+		{ _field_version_greater, _engine_type_haloreach, 3 },
 		{ _field_custom, "material" },
 		{ _field_custom },
 		{ _field_struct, "actual material\?", &material_struct_struct_definition },
+
 		{ _field_custom },
+
+		{ _field_version_less_or_equal, _engine_type_haloreach },
+		{ _field_long_integer, "unknown" }, // likely flags?
+
 		{ _field_real_bounds, "radius:world units" },
 		{ _field_real_bounds, "decay time:seconds!" },
 		{ _field_real_bounds, "lifespan:seconds!" },
+
+		{ _field_version_greater, _engine_type_haloreach, 2 },
 		{ _field_real, "clamp angle:degrees#Projections at greater than this angle will be clamped to this angle" },
 		{ _field_real, "cull angle:degrees#Projections at greater than this angle will not be drawn" },
+
 		{ _field_real, "depth bias" },
 		{ _field_custom, "Decal Strip" },
 		{ _field_real, "strip U-coordinate tile length" },
@@ -34,11 +48,22 @@ namespace blofeld
 	{
 		{ _field_long_flags, "flags", &decal_system_flags },
 		{ _field_long_integer, "ring buffer size (in triangles)#bigger sizes keep more decals around but use much more memory" },
+
+		{ _field_version_less_or_equal, _engine_type_haloreach },
+		{ _field_real, "unknown@"},
+
 		{ _field_long_integer, "ring buffer size single player (in triangles)#above entry is for MP -- SP can be different" },
 		{ _field_real, "material shader fade time#material shader lifetime is modulated from 1 to 0 over this time" },
+
+		{ _field_version_less_or_equal, _engine_type_haloreach },
+		{ _field_tag_reference, "successor", &decal_system_reference },
+
 		{ _field_real_point_2d, "decal scale override#NOTE - If set to non-zero, this will override manual scaling in Sapien and smash it with these values" },
+
+		{ _field_version_greater, _engine_type_haloreach, 2 },
 		{ _field_explanation, "Decal bucket sharing", "For the index bounds below, if non-zero, sprites will be chosen between low and high bounds, inclusive.\nUse this to keep multiple different decals in the same bucket.\nIf multiple decal systems use the same bitmap set and the same material definition, they will be combined at runtime and share their vertex bucket.\nThis is more efficient in memory and more performant to render." },
 		{ _field_short_bounds, "random sprite index bounds#if zero, will use random sprite" },
+		
 		{ _field_block, "decals", &decal_definition_block_block },
 		{ _field_real, "runtime max radius!" },
 		{ _field_terminator }
