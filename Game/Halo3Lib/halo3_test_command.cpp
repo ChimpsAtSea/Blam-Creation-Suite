bc6513315a9e3d78d05bab76a8b5c8a08dd98e7a
theTwist84 <lee.foulger.94@gmail.com>
2020-07-19 12:41:40 +0100

    Added a new test command

--- Game/Halo3Lib/halo3_test_command.cpp
+++ Game/Halo3Lib/halo3_test_command.cpp
@@ -8,6 +8,9 @@
 /* ---------- private prototypes */
 /* ---------- public code */
 
+void print_mainmenu_ui_globals_test_command();
+void first_person_weapon_offset_test_command();
+
 c_halo3_test_command::c_halo3_test_command()
 {
 	c_console::register_command(k_halo3_test_command_name, this);
@@ -18,14 +21,76 @@ c_halo3_test_command::~c_halo3_test_command()
 	c_console::unregister_command(k_halo3_test_command_name);
 }
 
+bool c_halo3_test_command::execute_command(const std::vector<std::string>& arguments)
+{
+	if (arguments.empty()) return true;
+
+	if (!arguments.front().compare(k_halo3_test_command_name))
+	{
+		if (arguments.size() >= 2)
+		{
+			const std::string& arg1 = arguments[1];
+
+			c_console::set_text_color(_console_color_info);
+
+			if (!arg1.compare("print_mainmenu_ui_globals"))
+			{
+				print_mainmenu_ui_globals_test_command();
+			}
+			else if (!arg1.compare("first_person_weapon_offset"))
+			{
+				first_person_weapon_offset_test_command();
+			}
+		}
+		else return false;
+	}
+	return true;
+}
+
+std::string c_halo3_test_command::get_command_info(const std::string& topic) const
+{
+	if (!topic.empty())
+	{
+		if (!topic.compare(k_halo3_test_command_name))
+		{
+			return
+				"test\n"
+				"Usage: test <command>\n"
+				"Commands:\n"
+				"\tprint_mainmenu_ui_globals\n";
+				"\tfirst_person_weapon_offset\n";
+		}
+	}
+	return "";
+}
+
+std::string c_halo3_test_command::get_command_auto_complete(const std::vector<std::string>& Arguments) const
+{
+	if (Arguments.size() > 1)
+	{
+		if (!Arguments[1].compare("p"))
+		{
+			return "print_mainmenu_ui_globals";
+		}
+		else if (!Arguments[1].compare("f"))
+		{
+			return "first_person_weapon_offset";
+		}
+	}
+
+	return "";
+}
+
+/* ---------- private code */
+
 extern char* halo3_tag_definition_get(unsigned long group, const char* tag_name);
 extern char* halo3_tag_address_get(uint32_t tag_instance_address);
 extern char* halo3_tag_definition_get(uint32_t index);
 extern const char* halo3_tag_name_get(unsigned long tag_index);
 
-void print_mainmenu_ui_globals()
+void print_mainmenu_ui_globals_test_command()
 {
-	if (char *globals = halo3_tag_definition_get(blofeld::GLOBALS_TAG, "globals\\globals"); globals)
+	if (char* globals = halo3_tag_definition_get(blofeld::GLOBALS_TAG, "globals\\globals"); globals)
 	{
 		if (unsigned long interface_tag_block_count = *reinterpret_cast<unsigned long*>(&globals[0x134] + 0); interface_tag_block_count)
 		{
@@ -58,55 +123,81 @@ void print_mainmenu_ui_globals()
 	}
 }
 
-bool c_halo3_test_command::execute_command(const std::vector<std::string>& arguments)
+char* weapon_get(const char* tag_name)
 {
-	if (arguments.empty()) return true;
-
-	if (!arguments.front().compare(k_halo3_test_command_name))
+	if (char* weapon = halo3_tag_definition_get(blofeld::WEAPON_TAG, tag_name); weapon)
 	{
-		if (arguments.size() >= 2)
-		{
-			const std::string& arg1 = arguments[1];
-
-			c_console::set_text_color(_console_color_info);
-
-			if (!arg1.compare("print_mainmenu_ui_globals"))
-			{
-				print_mainmenu_ui_globals();
-			}
-		}
-		else return false;
+		return weapon;
 	}
-	return true;
+	return nullptr;
 }
 
-std::string c_halo3_test_command::get_command_info(const std::string& topic) const
+real_vector3d& first_person_weapon_offset_get(bool centered, const char* tag_name)
 {
-	if (!topic.empty())
+	real_vector3d default_offset = {};
+	if (char* weapon = weapon_get(tag_name); weapon)
 	{
-		if (!topic.compare(k_halo3_test_command_name))
-		{
-			return
-				"Update test\n"
-				"Usage: test <command>\n"
-				"Commands: print_mainmenu_ui_globals";
-		}
+		return *reinterpret_cast<real_vector3d*>(&weapon[!centered ? 0x4D8 : 0x4E4]);
 	}
-	return "";
+	return default_offset;
 }
 
-std::string c_halo3_test_command::get_command_auto_complete(const std::vector<std::string>& Arguments) const
+void first_person_weapon_offset_test_command()
 {
-	if (Arguments.size() > 1)
-	{
-		if (!Arguments[1].compare("p"))
-		{
-			return "print_mainmenu_ui_globals";
-		}
-	}
+	real_vector3d& energy_blade_lowered_fp_weapon_offset = first_person_weapon_offset_get(false, "objects\\weapons\\melee\\energy_blade\\energy_blade");
+	real_vector3d& energy_blade_centered_fp_weapon_offset = first_person_weapon_offset_get(true, "objects\\weapons\\melee\\energy_blade\\energy_blade");
+	real_vector3d& energy_blade_useless_lowered_fp_weapon_offset = first_person_weapon_offset_get(false, "objects\\weapons\\melee\\energy_blade\\energy_blade_useless");
+	real_vector3d& energy_blade_useless_centered_fp_weapon_offset = first_person_weapon_offset_get(true, "objects\\weapons\\melee\\energy_blade\\energy_blade_useless");
+	real_vector3d& gravity_hammer_lowered_fp_weapon_offset = first_person_weapon_offset_get(false, "objects\\weapons\\melee\\gravity_hammer\\gravity_hammer");
+	real_vector3d& gravity_hammer_centered_fp_weapon_offset = first_person_weapon_offset_get(true, "objects\\weapons\\melee\\gravity_hammer\\gravity_hammer");
+	real_vector3d& assault_bomb_lowered_fp_weapon_offset = first_person_weapon_offset_get(false, "objects\\weapons\\multiplayer\\assault_bomb\\assault_bomb");
+	real_vector3d& assault_bomb_centered_fp_weapon_offset = first_person_weapon_offset_get(true, "objects\\weapons\\multiplayer\\assault_bomb\\assault_bomb");
+	real_vector3d& ball_lowered_fp_weapon_offset = first_person_weapon_offset_get(false, "objects\\weapons\\multiplayer\\ball\\ball");
+	real_vector3d& ball_centered_fp_weapon_offset = first_person_weapon_offset_get(true, "objects\\weapons\\multiplayer\\ball\\ball");
+	real_vector3d& flag_lowered_fp_weapon_offset = first_person_weapon_offset_get(false, "objects\\weapons\\multiplayer\\flag\\flag");
+	real_vector3d& flag_centered_fp_weapon_offset = first_person_weapon_offset_get(true, "objects\\weapons\\multiplayer\\flag\\flag");
+	real_vector3d& excavator_lowered_fp_weapon_offset = first_person_weapon_offset_get(false, "objects\\weapons\\pistol\\excavator\\excavator");
+	real_vector3d& excavator_centered_fp_weapon_offset = first_person_weapon_offset_get(true, "objects\\weapons\\pistol\\excavator\\excavator");
+	real_vector3d& magnum_lowered_fp_weapon_offset = first_person_weapon_offset_get(false, "objects\\weapons\\pistol\\magnum\\magnum");
+	real_vector3d& magnum_centered_fp_weapon_offset = first_person_weapon_offset_get(true, "objects\\weapons\\pistol\\magnum\\magnum");
+	real_vector3d& needler_lowered_fp_weapon_offset = first_person_weapon_offset_get(false, "objects\\weapons\\pistol\\needler\\needler");
+	real_vector3d& needler_centered_fp_weapon_offset = first_person_weapon_offset_get(true, "objects\\weapons\\pistol\\needler\\needler");
+	real_vector3d& plasma_pistol_lowered_fp_weapon_offset = first_person_weapon_offset_get(false, "objects\\weapons\\pistol\\plasma_pistol\\plasma_pistol");
+	real_vector3d& plasma_pistol_centered_fp_weapon_offset = first_person_weapon_offset_get(true, "objects\\weapons\\pistol\\plasma_pistol\\plasma_pistol");
+	real_vector3d& assault_rifle_lowered_fp_weapon_offset = first_person_weapon_offset_get(false, "objects\\weapons\\rifle\\assault_rifle\\assault_rifle");
+	real_vector3d& assault_rifle_centered_fp_weapon_offset = first_person_weapon_offset_get(true, "objects\\weapons\\rifle\\assault_rifle\\assault_rifle");
+	real_vector3d& battle_rifle_lowered_fp_weapon_offset = first_person_weapon_offset_get(false, "objects\\weapons\\rifle\\battle_rifle\\battle_rifle");
+	real_vector3d& battle_rifle_centered_fp_weapon_offset = first_person_weapon_offset_get(true, "objects\\weapons\\rifle\\battle_rifle\\battle_rifle");
+	real_vector3d& beam_rifle_lowered_fp_weapon_offset = first_person_weapon_offset_get(false, "objects\\weapons\\rifle\\beam_rifle\\beam_rifle");
+	real_vector3d& beam_rifle_centered_fp_weapon_offset = first_person_weapon_offset_get(true, "objects\\weapons\\rifle\\beam_rifle\\beam_rifle");
+	real_vector3d& covenant_carbine_lowered_fp_weapon_offset = first_person_weapon_offset_get(false, "objects\\weapons\\rifle\\covenant_carbine\\covenant_carbine");
+	real_vector3d& covenant_carbine_centered_fp_weapon_offset = first_person_weapon_offset_get(true, "objects\\weapons\\rifle\\covenant_carbine\\covenant_carbine");
+	real_vector3d& plasma_rifle_lowered_fp_weapon_offset = first_person_weapon_offset_get(false, "objects\\weapons\\rifle\\plasma_rifle\\plasma_rifle");
+	real_vector3d& plasma_rifle_centered_fp_weapon_offset = first_person_weapon_offset_get(true, "objects\\weapons\\rifle\\plasma_rifle\\plasma_rifle");
+	real_vector3d& shotgun_lowered_fp_weapon_offset = first_person_weapon_offset_get(false, "objects\\weapons\\rifle\\shotgun\\shotgun");
+	real_vector3d& shotgun_centered_fp_weapon_offset = first_person_weapon_offset_get(true, "objects\\weapons\\rifle\\shotgun\\shotgun");
+	real_vector3d& smg_lowered_fp_weapon_offset = first_person_weapon_offset_get(false, "objects\\weapons\\rifle\\smg\\smg");
+	real_vector3d& smg_centered_fp_weapon_offset = first_person_weapon_offset_get(true, "objects\\weapons\\rifle\\smg\\smg");
+	real_vector3d& sniper_rifle_lowered_fp_weapon_offset = first_person_weapon_offset_get(false, "objects\\weapons\\rifle\\sniper_rifle\\sniper_rifle");
+	real_vector3d& sniper_rifle_centered_fp_weapon_offset = first_person_weapon_offset_get(true, "objects\\weapons\\rifle\\sniper_rifle\\sniper_rifle");
+	real_vector3d& spike_rifle_lowered_fp_weapon_offset = first_person_weapon_offset_get(false, "objects\\weapons\\rifle\\spike_rifle\\spike_rifle");
+	real_vector3d& spike_rifle_centered_fp_weapon_offset = first_person_weapon_offset_get(true, "objects\\weapons\\rifle\\spike_rifle\\spike_rifle");
+	real_vector3d& flak_cannon_lowered_fp_weapon_offset = first_person_weapon_offset_get(false, "objects\\weapons\\support_high\\flak_cannon\\flak_cannon");
+	real_vector3d& flak_cannon_centered_fp_weapon_offset = first_person_weapon_offset_get(true, "objects\\weapons\\support_high\\flak_cannon\\flak_cannon");
+	real_vector3d& rocket_launcher_lowered_fp_weapon_offset = first_person_weapon_offset_get(false, "objects\\weapons\\support_high\\rocket_launcher\\rocket_launcher");
+	real_vector3d& rocket_launcher_centered_fp_weapon_offset = first_person_weapon_offset_get(true, "objects\\weapons\\support_high\\rocket_launcher\\rocket_launcher");
+	real_vector3d& spartan_laser_lowered_fp_weapon_offset = first_person_weapon_offset_get(false, "objects\\weapons\\support_high\\spartan_laser\\spartan_laser");
+	real_vector3d& spartan_laser_centered_fp_weapon_offset = first_person_weapon_offset_get(true, "objects\\weapons\\support_high\\spartan_laser\\spartan_laser");
+	real_vector3d& brute_shot_lowered_fp_weapon_offset = first_person_weapon_offset_get(false, "objects\\weapons\\support_low\\brute_shot\\brute_shot");
+	real_vector3d& brute_shot_centered_fp_weapon_offset = first_person_weapon_offset_get(true, "objects\\weapons\\support_low\\brute_shot\\brute_shot");
+	real_vector3d& sentinel_gun_lowered_fp_weapon_offset = first_person_weapon_offset_get(false, "objects\\weapons\\support_low\\sentinel_gun\\sentinel_gun");
+	real_vector3d& sentinel_gun_centered_fp_weapon_offset = first_person_weapon_offset_get(true, "objects\\weapons\\support_low\\sentinel_gun\\sentinel_gun");
+	real_vector3d& machinegun_turret_lowered_fp_weapon_offset = first_person_weapon_offset_get(false, "objects\\weapons\\turret\\machinegun_turret\\machinegun_turret");
+	real_vector3d& machinegun_turret_centered_fp_weapon_offset = first_person_weapon_offset_get(true, "objects\\weapons\\turret\\machinegun_turret\\machinegun_turret");
+	real_vector3d& machinegun_turret_integrated_lowered_fp_weapon_offset = first_person_weapon_offset_get(false, "objects\\weapons\\turret\\machinegun_turret\\machinegun_turret_integrated");
+	real_vector3d& machinegun_turret_integrated_centered_fp_weapon_offset = first_person_weapon_offset_get(true, "objects\\weapons\\turret\\machinegun_turret\\machinegun_turret_integrated");
+	real_vector3d& missile_pod_lowered_fp_weapon_offset = first_person_weapon_offset_get(false, "objects\\weapons\\turret\\missile_pod\\missile_pod");
+	real_vector3d& missile_pod_centered_fp_weapon_offset = first_person_weapon_offset_get(true, "objects\\weapons\\turret\\missile_pod\\missile_pod");
 
-	return "";
+	debug_point;
 }
-
-/* ---------- private code */
-
