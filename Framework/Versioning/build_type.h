8c7bbb40b51690a67ec2f6d57ecc5ef0f83037ed
theTwist84 <lee.foulger.94@gmail.com>
2020-08-17 23:27:09 +0100

    Added the new build version

--- Framework/Versioning/build_type.h
+++ Framework/Versioning/build_type.h
@@ -31,6 +31,7 @@ enum e_build : uint64_t
 	_build_mcc_1_1658_0_0 = MAKE_FILE_VERSION(1, 1658, 0, 0),
 	_build_mcc_1_1698_0_0 = MAKE_FILE_VERSION(1, 1698, 0, 0),
 	_build_mcc_1_1716_0_0 = MAKE_FILE_VERSION(1, 1716, 0, 0),
+	_build_mcc_1_1767_0_0 = MAKE_FILE_VERSION(1, 1767, 0, 0),
 	_build_eldorado_1_106708_cert_ms23 = MAKE_PRODUCT_VERSION(0, 1, 106708), // 1.106708 cert_ms23  Mar 20 2015 17:24:42
 	_build_eldorado_1_155080_cert_ms23 = MAKE_PRODUCT_VERSION(0, 1, 155080), // #TODO: If these are required for boot we must find a way to accurately find the version from the binary
 	_build_eldorado_1_171227_cert_ms23 = MAKE_PRODUCT_VERSION(0, 1, 171227), // #TODO: If these are required for boot we must find a way to accurately find the version from the binary
@@ -87,6 +88,7 @@ constexpr string_type get_enum_string(e_build build)
 	enum_string_ex(string_type, pretty_string, build, _build_mcc_1_1658_0_0, "MCC 1.1658.0.0");
 	enum_string_ex(string_type, pretty_string, build, _build_mcc_1_1698_0_0, "MCC 1.1698.0.0");
 	enum_string_ex(string_type, pretty_string, build, _build_mcc_1_1716_0_0, "MCC 1.1716.0.0");
+	enum_string_ex(string_type, pretty_string, build, _build_mcc_1_1767_0_0, "MCC 1.1767.0.0");
 	enum_string_ex(string_type, pretty_string, build, _build_eldorado_1_106708_cert_ms23, "Eldorado 1.106708 cert_ms23");
 	enum_string_ex(string_type, pretty_string, build, _build_eldorado_1_155080_cert_ms23, "Eldorado 1.155080 cert_ms23");
 	enum_string_ex(string_type, pretty_string, build, _build_eldorado_1_171227_cert_ms23, "Eldorado 1.171227 cert_ms23");
