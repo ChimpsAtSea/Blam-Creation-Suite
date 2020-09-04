768a15bc445584a67445f9c46e7cdf096a74a7aa
theTwist84 <lee.foulger.94@gmail.com>
2020-09-04 01:25:41 +0100

    Versioning oopsie doopsie

--- Framework/Versioning/game_context_version.cpp
+++ Framework/Versioning/game_context_version.cpp
@@ -1,2 +1,21 @@
 #include "versioning-private-pch.h"
-#include "game_context_version.h"
+
+e_game_context_version get_game_context_version_from_build(e_build build)
+{
+	if (build >= _build_mcc_1_1767_0_0)
+	{
+		return _game_context_version_4;
+	}
+	else if (build >= _build_mcc_1_1367_0_0)
+	{
+		return _game_context_version_3;
+	}
+	else if (build >= _build_mcc_1_1186_0_0)
+	{
+		return _game_context_version_2;
+	}
+	else
+	{
+		return _game_context_version_1;
+	}
+}
--- Framework/Versioning/game_context_version.h
+++ Framework/Versioning/game_context_version.h
@@ -8,22 +8,4 @@ enum e_game_context_version
 	_game_context_version_4,
 };
 
-constexpr e_game_context_version get_game_context_version_from_build(e_build build)
-{
-	if (build >= _build_mcc_1_1767_0_0)
-	{
-		return _game_context_version_4;
-	}
-	else if (build >= _build_mcc_1_1716_0_0)
-	{
-		return _game_context_version_3;
-	}
-	else if (build >= _build_mcc_1_1186_0_0)
-	{
-		return _game_context_version_2;
-	}
-	else
-	{
-		return _game_context_version_1;
-	}
-}
+e_game_context_version get_game_context_version_from_build(e_build build);
