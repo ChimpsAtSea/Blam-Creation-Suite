acc61322422fd027fa51bf42e1acd784350ad01b
theTwist84 <lee.foulger.94@gmail.com>
2020-11-18 01:42:13 +0000

    Added the new build version (1.1955.0.0)

--- Framework/GameFramework/MCC/IGameEngineHost.cpp
+++ Framework/GameFramework/MCC/IGameEngineHost.cpp
@@ -658,6 +658,7 @@ bool IGameEngineHost::PlayerConfigurationFromBuild(e_build build, c_player_confi
 		break;
 	case _build_mcc_1_1896_0_0:
 	case _build_mcc_1_1930_0_0:
+	case _build_mcc_1_1955_0_0:
 	default:
 		s_player_configuration = new c_player_configuration(_player_configuration_version_9);
 		break;
