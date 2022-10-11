974b39938a5dd3f759edccaea7f544ccb96a0eeb
Haydn Trigg <me@haydntrigg.com>
2022-10-11 05:08:21 +0100

    Fix C# Project Warnings

--- Tags/ChimpLib/Commands/HelloWorld.cs
+++ Tags/ChimpLib/Commands/HelloWorld.cs
@@ -6,6 +6,8 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+
 namespace ChimpsAtSea.BlamCreationSuite.Chimp.Commands
 {
     public class HelloWorldCommand : ICommand
