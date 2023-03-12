77d4bc61bf89e7a408f7983bab7bcced8ec62dbb
Haydn Trigg <me@haydntrigg.com>
2023-03-12 23:02:53 +0000

    Mandrill Python Stub Callback Test

--- bcs/bcs_python.cpp
+++ bcs/bcs_python.cpp
@@ -1,6 +1,16 @@
 #include "bcs.h"
+#include <stdio.h>
 
 BCS_SHARED extern "C" int python_test(int a, int b)
 {
 	return a + b;
+}
+
+// Define the prototype of the callback function in C
+typedef void (*callback_type)(int);
+
+// Define a function that takes a callback and calls it with the value 42
+BCS_SHARED extern "C" void call_with_callback(callback_type callback) {
+    printf("Calling callback with value 42...\n");
+    callback(42);
 }
--- bcs/mandrill.py
+++ bcs/mandrill.py
@@ -15,3 +15,33 @@ lib.python_test.restype = ctypes.c_int
 # Call python_test from Python
 result = lib.python_test(1, 2)
 print(result)
+
+
+# Define the callback function in Python
+def my_callback(x):
+    print("The value of x is:", x)
+
+# Define the prototype of the C function that will call the callback
+callback_type = ctypes.CFUNCTYPE(None, ctypes.c_int)
+
+# Create a function pointer that points to the Python callback function
+callback_func = callback_type(my_callback)
+
+
+
+
+
+# Get a reference to the C function that will call the callback
+call_with_callback = lib.call_with_callback
+
+# Set the argument types for the C function
+call_with_callback.argtypes = [callback_type]
+
+
+
+
+# Call the C function with the callback
+call_with_callback(callback_func)
+
+
+
