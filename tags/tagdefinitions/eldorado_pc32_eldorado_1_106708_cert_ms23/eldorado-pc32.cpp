2d37a5a923ece4782fdc5a93443b9b737d035e1d
Haydn Trigg <me@haydntrigg.com>
2023-09-08 06:17:26 +0930

    Eldorado Definitions Improvements

--- tags/tagdefinitions/eldorado_pc32_eldorado_1_106708_cert_ms23/eldorado-pc32.cpp
+++ tags/tagdefinitions/eldorado_pc32_eldorado_1_106708_cert_ms23/eldorado-pc32.cpp
@@ -15007,10 +15007,7 @@ namespace blofeld::eldorado::pc32
 		{ _field_long_integer, "size" },
 		{ _field_struct, "checksum", &blofeld::eldorado::pc32::resource_checksum_struct },
 		{ _field_short_integer, "resource reference count" },
-		{ _field_short_block_index, "streaming sublocation table", &blofeld::eldorado::pc32::cache_file_resource_streaming_sublocation_table_block_block },
-		{ _field_long_integer, "value" },
-		{ _field_long_integer, "value" },
-		{ _field_long_integer, "value" },
+		{ _field_struct, "streaming sublocation table", &blofeld::eldorado::pc32::cache_file_resource_streaming_sublocation_table_block },
 		{ _field_terminator }
 	};
 
